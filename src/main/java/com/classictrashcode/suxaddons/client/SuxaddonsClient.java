f61608b3c3cd0346d6d435921764d8a66f432cb3
classictrashlike <classictrashcode@gmail.com>
2026-03-15 17:39:46 +0300

    CinderbatTracker will add Tracers later.

--- src/main/java/com/classictrashcode/suxaddons/client/SuxaddonsClient.java
+++ src/main/java/com/classictrashcode/suxaddons/client/SuxaddonsClient.java
@@ -2,13 +2,14 @@ package com.classictrashcode.suxaddons.client;
 
 import com.classictrashcode.suxaddons.client.commands.CommandManager;
 import com.classictrashcode.suxaddons.client.config.ConfigManager;
+import com.classictrashcode.suxaddons.client.hunting.CinderBatTracker;
+import com.classictrashcode.suxaddons.client.hunting.CinderBatTrackerHud;
 import com.classictrashcode.suxaddons.client.hunting.HideonLeafTracker;
 import com.classictrashcode.suxaddons.client.hunting.HideonLeafTrackerHud;
 import com.classictrashcode.suxaddons.client.macros.ChatMacros;
 import com.classictrashcode.suxaddons.client.utils.ChatUtils;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 
 public class SuxaddonsClient implements ClientModInitializer {
@@ -20,8 +21,10 @@ public class SuxaddonsClient implements ClientModInitializer {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             ChatUtils.tick();
             HideonLeafTracker.tick();
+            CinderBatTracker.tick();
             ChatMacros.tick(client.getWindow().handle());
         });
         HudRenderCallback.EVENT.register(new HideonLeafTrackerHud());
+        HudRenderCallback.EVENT.register(new CinderBatTrackerHud());
     }
 }
